1911ef5235359dc66c2131dd634b0b35a23476a1
Shun Wang <shunwang@amazon.com>
2014-10-26 16:54:57 +0000

    add EC2 and SQS demo code

---
+++ AwsEc2Demo.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2010-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
+
+/**
+ * Welcome to your new AWS Java SDK based project!
+ *
+ * This class is meant as a starting point for your console-based application that
+ * makes one or more calls to the AWS services supported by the Java SDK, such as EC2,
+ * SimpleDB, and S3.
+ *
+ * In order to use the services in this sample, you need:
+ *
+ *  - A valid Amazon Web Services account. You can register for AWS at:
+ *       https://aws-portal.amazon.com/gp/aws/developer/registration/index.html
+ *
+ *  - Your account's Access Key ID and Secret Access Key:
+ *       http://aws.amazon.com/security-credentials
+ *
+ *  - A subscription to Amazon EC2. You can sign up for EC2 at:
+ *       http://aws.amazon.com/ec2/
+ *
+ *  - A subscription to Amazon SimpleDB. You can sign up for Simple DB at:
+ *       http://aws.amazon.com/simpledb/
+ *
+ *  - A subscription to Amazon S3. You can sign up for S3 at:
+ *       http://aws.amazon.com/s3/
+ */
+public class AwsEc2Demo {
+
+    /*
+     * Before running the code:
+     *      Fill in your AWS access credentials in the provided credentials
+     *      file template, and be sure to move the file to the default location
+     *      (~/.aws/credentials) where the sample code will load the
+     *      credentials from.
+     *      https://console.aws.amazon.com/iam/home?#security_credential
+     *
+     * WANRNING:
+     *      To avoid accidental leakage of your credentials, DO NOT keep
+     *      the credentials file in your source directory.
+     */
+
+    static AmazonEC2      ec2;
+
+    /**
+     * The only information needed to create a client are security credentials
+     * consisting of the AWS Access Key ID and Secret Access Key. All other
+     * configuration, such as the service endpoints, are performed
+     * automatically. Client parameters, such as proxies, can be specified in an
+     * optional ClientConfiguration object when constructing a client.
+     *
+     * @see com.amazonaws.auth.BasicAWSCredentials
+     * @see com.amazonaws.auth.PropertiesCredentials
+     * @see com.amazonaws.ClientConfiguration
+     */
+    private static void init() throws Exception {
+
+        /*
+         * The ProfileCredentialsProvider will return your [default]
+         * credential profile by reading from the credentials file located at
+         * (~/.aws/credentials).
+         */
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        ec2 = new AmazonEC2Client(credentials);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("===========================================");
+        System.out.println("Welcome to the AWS Java SDK!");
+        System.out.println("===========================================");
+
+        init();
+
+        /*
+         * Amazon EC2
+         *
+         * The AWS EC2 client allows you to create, delete, and administer
+         * instances programmatically.
+         *
+         * In this sample, we use an EC2 client to get a list of all the
+         * availability zones, and all instances sorted by reservation id.
+         */
+        try {
+            DescribeAvailabilityZonesResult availabilityZonesResult = ec2.describeAvailabilityZones();
+            System.out.println("You have access to " + availabilityZonesResult.getAvailabilityZones().size() +
+                    " Availability Zones.");
+
+            DescribeInstancesResult describeInstancesRequest = ec2.describeInstances();
+            List<Reservation> reservations = describeInstancesRequest.getReservations();
+            Set<Instance> instances = new HashSet<Instance>();
+
+            for (Reservation reservation : reservations) {
+                instances.addAll(reservation.getInstances());
+            }
+
+            System.out.println("You have " + instances.size() + " Amazon EC2 instance(s) running.");
+        } catch (AmazonServiceException ase) {
+                System.out.println("Caught Exception: " + ase.getMessage());
+                System.out.println("Reponse Status Code: " + ase.getStatusCode());
+                System.out.println("Error Code: " + ase.getErrorCode());
+                System.out.println("Request ID: " + ase.getRequestId());
+        }
+    }
+}
